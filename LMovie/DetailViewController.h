a0c0ba6f89be844fce3e815969482874202ecc3f
Jonathan Duss <Jo@MacBook-Pro-Jo.local>
2012-07-02 19:24:23 +0200

    Initial Commit

---
+++ LMovie/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  LMovie
+//
+//  Created by Jonathan Duss on 02.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
